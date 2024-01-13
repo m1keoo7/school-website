09bd2b13b9f9fa3ddfbe9a53e39ce4ba55903255
Mihail Vedernikov <mike007.dev@gmail.com>
2024-01-13 13:08:09 +0300

    feat: add new photos in gallery

--- data/contents.js
+++ data/contents.js
@@ -1,60 +1,162 @@
 const galleryContent = [
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+  {
+    path: "./images/image-18-10-23-09-09.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-1.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-2.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-3.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-4.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-5.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-6.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-7.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    },
-    {
-        path: "./images/image-18-10-23-09-09-7.jpeg",
-        title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-8.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`
-    }
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-9.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-10.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-11.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-12.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-13.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-14.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-15.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-16.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-17.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-18.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-19.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-20.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-21.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-22.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-23.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-24.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "./images/image-18-10-23-09-09-25.jpeg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
 ];
 
 export default {
-    galleryContent
+  galleryContent,
 };
