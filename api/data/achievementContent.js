d5e79192e892ce3aa78f8833a14c209754ddf7dc
Mihail Vedernikov <mike007.dev@gmail.com>
2024-01-14 21:55:40 +0300

    fix: change path source

--- api/data/achievementContent.js
+++ api/data/achievementContent.js
@@ -1,6 +1,90 @@
 const achievementContent = [
   {
-    path: "images/facets_of_talent/our_achievements/1.jpg",
+    path: "images/facets_of_talent/our_achievements/image1.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image2.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image3.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image4.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image5.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image6.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image7.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image8.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image9.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image10.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image11.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image12.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image13.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image14.jpg",
+    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
+        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
+        sed?`,
+  },
+  {
+    path: "images/facets_of_talent/our_achievements/image15.jpg",
     title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
         harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
         sed?`,
