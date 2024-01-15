7f21a8d408b1eb2e8819bfae8c3a3f3b293c5f51
Mihail Vedernikov <mike007.dev@gmail.com>
2024-01-15 20:47:20 +0300

    fix: change renderdata and button

--- api/data/achievementContent.js
+++ api/data/achievementContent.js
@@ -1,15 +1,9 @@
 const achievementContent = [
   {
     path: "images/facets_of_talent/our_achievements/image1.jpg",
-    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
-        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`,
   },
   {
     path: "images/facets_of_talent/our_achievements/image2.jpg",
-    title: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda deleniti dolore ea facilis
-        harum, hic magnam maiores minima nam necessitatibus neque numquam obcaecati optio quae quod quos, saepe,
-        sed?`,
   },
   {
     path: "images/facets_of_talent/our_achievements/image3.jpg",
--- renderData.js
+++ renderData.js
@@ -7,23 +7,21 @@ const inputDataName = `${nameContent}Content`;
 const inputData = content[inputDataName][inputDataName];
 
 if (Array.isArray(inputData)) {
-  const galleryItemTemplates = inputData.map(
-    (item) =>
-      `<div class="col">
+  const galleryItemTemplates = inputData.map((item) => {
+    const titleContent = item.title
+      ? `<p class="card-text">${item.title}</p>`
+      : "";
+    return `<div class="col">
       <div class="card shadow-sm">
         <img src=${item.path} width="100%" height="100%" alt="image"/>
-        <div class="card-body">
-          <p class="card-text">${item.title}</p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-primary">Просмотр</button>
-              
-            </div>
+        <div class="card-body">${titleContent}
+          <div class="d-flex justify-content-center align-items-center">
+              <button type="button" class="btn btn-sm btn-primary w-75">Просмотр</button>
           </div>
         </div>
       </div>
-    </div>`,
-  );
+    </div>`;
+  });
 
   box.insertAdjacentHTML("beforeend", galleryItemTemplates.join(""));
 }
