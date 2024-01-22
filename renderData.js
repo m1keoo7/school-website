026d1c166603dccad5597d9aecf587e8f0ab9179
Mihail Vedernikov <mike007.dev@gmail.com>
2024-01-22 08:23:33 +0300

    feat: add modal

--- renderData.js
+++ renderData.js
@@ -5,6 +5,24 @@ const nameContent = box.dataset.name_content;
 const inputDataName = `${nameContent}Content`;
 
 const inputData = content[inputDataName][inputDataName];
+const modal = document.querySelector(".custom_modal");
+const modalContent = document.querySelector(".custom_modal-content");
+const closeBtn = document.querySelector(".close");
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none"; // Скрытие модального окна при клике вне изображения
+  }
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none"; // Скрытие модального окна при клике на кнопку закрытия
+});
+
+window.handleOpenModal = function (item) {
+  modalContent.src = item.dataset.path;
+  modal.style.display = "block";
+};
 
 if (Array.isArray(inputData)) {
   const galleryItemTemplates = inputData.map((item) => {
@@ -16,7 +34,7 @@ if (Array.isArray(inputData)) {
         <img src=${item.path} width="100%" height="100%" alt="image"/>
         <div class="card-body">${titleContent}
           <div class="d-flex justify-content-center align-items-center">
-              <button type="button" class="btn btn-sm btn-primary w-75">Просмотр</button>
+              <button type="button" class="btn btn-sm btn-primary w-75"  data-path="${item.path}" onclick="handleOpenModal(this)">Просмотр</button>
           </div>
         </div>
       </div>
